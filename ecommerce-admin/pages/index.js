ee5d9cb43a44de38b000eb75617cecadcb9eeb64
Nivaldo <nivalditin89@gmail.com>
2023-06-14 02:35:19 -0800

    nav_bar_added

--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -1,20 +1,9 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
 
-export default function Home() {
-  const {data:session} = useSession();
-
-  if (!session){
-    return (
-      <div className='bg-blue-900 w-screen h-screen flex items-center'>
-        <div className="text-center w-full">
-        <button onClick={()=>signIn("google")} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
-        </div>
-      </div>)
-  }
+export default function Home() { 
   return (
-    <div className="bg-blue-900 min-h-screen">
-      <div>logged in {session.user.email}</div>
-    </div>
-    
+    <Layout>
+      test
+    </Layout>
   )
 }
